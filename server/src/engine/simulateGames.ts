b883448dac0459b3c08ed32b3d17e0f0a25f9eaa
Zach <zowen17@gmail.com>
2026-03-01 23:20:48 -0500

    added code to siming a game and possessions

---
+++ server/src/engine/simulateGames.ts
@@ -0,0 +1,43 @@
+import { GameState } from "../domain/GameState";
+import { resolvePossession } from "./resolution/ResolvePossession";
+import { SeededRNG } from "./rng/seedRNG";
+import { simulatePossession } from "./SimulatePossession";
+
+export interface SimulationResult {
+    finalScore: {
+        home: number;
+        away: number;
+    };
+    totalPossessions: number;
+    boxScore: Record<string, any>;
+    gameState: GameState;
+}
+
+export function simulateGame(
+    initialState: GameState,
+    seed: number
+): SimulationResult {
+    const rng = new SeededRNG(seed);
+
+    let state = { ...initialState };
+    let totalPossessions = 0;
+
+    while (!isGameOver(state)) {
+        state = simulatePossession(state, rng);
+        totalPossessions++;
+    }
+
+    return {
+        finalScore: {
+            home: state.score.home,
+            away: state.score.away,
+        },
+        totalPossessions,
+        boxScore: state.boxScore,
+        gameState: state
+    };
+}
+
+function isGameOver(state: GameState): boolean {
+    return state.clock <= 0;
+}
